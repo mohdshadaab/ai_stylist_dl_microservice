ffb6c1061f7ec523bb9b499a680b8f85bd5a543b
mdshadaab <mohdshadaab27@gmail.com>
2022-04-25 19:13:04 +0530

    updated model to u2net

--- early_jobs.sh
+++ early_jobs.sh
@@ -1,2 +1,3 @@
 gdown --id 1T5V3Fxj3BEmAZB9T51zWoCqCkI4i1Fae
-gdown --id 1HFTJXvYDWlK-eLseamXWnmJ0Yr7ln4Ei --output saved_models/u2netp/u2netp.pth
+#gdown --id 1HFTJXvYDWlK-eLseamXWnmJ0Yr7ln4Ei --output saved_models/u2netp/u2netp.pth
+gdown --id 1HFTJXvYDWlK-eLseamXWnmJ0Yr7ln4Ei -output saved_models/u2net/u2net.pth
