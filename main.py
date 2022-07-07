95890963bf3ab0364cc24fe7817ccbb28ecc36e0
Mohammad Shadaab <mohdshadaab27@gmail.com>
2022-07-08 00:26:58 +0530

    Update main.py
    Added localhost 3000 to orgin

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ origins = [
     "https://localhost.tiangolo.com",
     "http://localhost",
     "http://localhost:8080",
+    "http://localhost:3000"
 ]
 
 #to handle requests between cross-origin resources
